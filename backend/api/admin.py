87c5461c1ea4cfd8066ea0cfce61f809c072e5f4
Daniil Satnichenka <daniil.satnichenka@gmail.com>
2022-08-30 12:16:58 +0400

    added search

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -5,16 +5,21 @@ from .models import Cart, Favorite, Ingredient, Recipe, Tag
 
 class TagAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug', 'color')
+    search_fields = ('name')
 
 
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('name', 'measurement_unit')
     list_filter = ('name',)
+    search_fields = ('name')
+
 
 
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ('name', 'author', 'count_favorites')
     list_filter = ('author', 'name', 'tags')
+    search_fields = ('name')
+
 
     def count_favorites(self, obj):
         return obj.favorites.count()

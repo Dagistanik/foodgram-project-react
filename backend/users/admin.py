c47e1f166da262c9894589d2a0489f84f69a7ce4
Daniil Satnichenka <daniil.satnichenka@gmail.com>
2022-08-30 12:43:41 +0400

    fixeded all admin.py

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from users.models import Follow
 
-admin.site.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('user', 'author')
+    list_filter = ('user')
+    search_fields = ('user')
+
+
+admin.site.register(Follow, FollowAdmin)

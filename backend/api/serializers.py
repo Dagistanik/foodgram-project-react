0ff8828794dbc5e4829fe38f4f9ee1d5fe693e8a
Daniil Satnichenka <daniil.satnichenka@gmail.com>
2022-08-31 13:23:32 +0400

    второе ревью

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -3,7 +3,6 @@ from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 
-
 from api.models import Ingredient, IngredientAmount, Recipe, Tag
 from users.models import Follow
 from users.serializers import CustomUserSerializer

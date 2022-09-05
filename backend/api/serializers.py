1293eb9076b8809878058d2e23efae162ef3b26a
Daniil Satnichenka <daniil.satnichenka@gmail.com>
2022-09-05 20:27:26 +0400

    final2

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -40,7 +40,7 @@ class IngredientAmountSerializer(serializers.ModelSerializer):
         validators = (
             UniqueTogetherValidator(
                 queryset=IngredientAmount.objects.all(),
-                fields=('ingredient', 'recipe')
+                fields=('ingridients', 'recipe')
             ),
         )
 
